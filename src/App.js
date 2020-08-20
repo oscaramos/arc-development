3119f69003fe1152d344a47e877c91d0302873c9
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-19 19:54:19 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route path='/services' component={() => <div>Services</div>} />
+          <Route path='/customsoftware' component={() => <div>Custom software</div>} />
+          <Route path='/mobileapps' component={() => <div>Mobile apps</div>} />
+          <Route path='/revolution' component={() => <div>Revolution</div>} />
+          <Route path='/about' component={() => <div>About</div>} />
+          <Route path='/contact' component={() => <div>Contact</div>} />
+          <Route path='/estimate' component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
