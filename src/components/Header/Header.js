b7aeac6ffb488a0821dea81e5cfb7c9747289f5c
Oscar <oscar.ramos@ucsp.edu.pe>
2020-08-19 19:54:30 -0500

    Header is now url aware

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
-import { makeStyles } from '@material-ui/core/styles'
-
-
-import logo from '../../assets/logo.svg'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
   const { children } = props
@@ -28,7 +29,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom: '3em'
   },
   logo: {
-    height: '7em'
+    height: '8em'
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -50,31 +57,49 @@ const useStyles = makeStyles(theme => ({
 
 // TODO: use disableGutters when possible
 // TODO: refactor typography when possible
+// TODO: MUI component over Others component
 
 const Header = () => {
   const classes = useStyles()
   const [value, setValue] = useState(0);
+  const location = useLocation()
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    console.log(location.pathname)
+    if (location.pathname === '/') setValue(0)
+    if (location.pathname === '/services') setValue(1)
+    if (location.pathname === '/revolution') setValue(2)
+    if (location.pathname === '/about') setValue(3)
+    if (location.pathname === '/contact') setValue(4)
+  }, [location.pathname])
 
   return (
     <>
       <ElevationScroll>
         <AppBar color='primary'>
           <Toolbar disableGutters>
-            <img alt='company logo' src={logo} className={classes.logo} />
+            <Button
+              component={Link}
+              to="/"
+              className={classes.logoContainer}
+              disableRipple
+            >
+              <img alt='company logo' src={logo} className={classes.logo} />
+            </Button>
             <Tabs value={value}
                   onChange={handleChange}
                   className={classes.tabContainer}
                   indicatorColor="primary"
             >
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Revolution" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
+              <Tab className={classes.tab} component={Link} to="/" label="Home" />
+              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
+              <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
+              <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
+              <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
             </Tabs>
 
             <Button variant="contained" color="secondary" className={classes.button}>
